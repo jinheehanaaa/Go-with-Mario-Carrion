6929f5c690569106c2c0760755960c81e1364593
Jinhee Han <80008300+jinheehanaaa@users.noreply.github.com>
2023-02-06 16:32:52 -0600

    012 - Functional Options
    Refactor (which introduces complexity)

--- Learning-Golang/code/012/EX-001/main.go
+++ Learning-Golang/code/012/EX-001/main.go
@@ -10,6 +10,34 @@ import (
 // 001. HTTP Server Implementation
 // curl -X GET http://localhost:8080/
 // curl -X GET http://127.0.0.1:8080/
+// 002. Change with pattern
+// Default config value from outisde of main
+// Use ServerOpts for Data Transfer Object (DTO) config
+
+type ServerOpts struct {
+	Address      string
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+}
+
+// Take default value from ServerOpts
+func NewServerOpts() ServerOpts {
+	return ServerOpts{
+		Address:      ":8080",
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+}
+
+func NewServer(opt ServerOpts) http.Server {
+	s := http.Server{
+		Addr:         opt.Address,
+		ReadTimeout:  opt.ReadTimeout,
+		WriteTimeout: opt.WriteTimeout,
+	}
+
+	return s
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -17,12 +45,9 @@ func main() {
 		fmt.Fprintf(w, "Hello")
 	})
 
-	s := http.Server{
-		Addr:         ":8080",
-		Handler:      mux,
-		ReadTimeout:  1 * time.Second,
-		WriteTimeout: 1 * time.Second,
-	}
+	s := NewServer(NewServerOpts())
+
+	s.Handler = mux
 
 	if err := s.ListenAndServe(); err != nil {
 		log.Fatalln("Couldn't listen and server", err)
