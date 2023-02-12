21b4ad2987e744cf6f2adf8147752045c3d9a35f
Jinhee Han <80008300+jinheehanaaa@users.noreply.github.com>
2023-02-11 21:11:32 -0600

    002 - Introduction to Protocol Buffers
    Writer as Go, Reader as Ruby

---
+++ System Design (gRPC, GaphQL and REST)/002 - Introduction to Protocol Buffers.md
@@ -0,0 +1,8 @@
+# Protocol Buffers
+- are a data format used to serialize structured data
+- uses an Interface Description Language to describe the structure of some data
+- includes a collection of programs to generate source code
+
+# Resources
+- [YT](https://www.youtube.com/watch?v=U69jAJYqdQs&list=PL7yAAGMOat_EX1nv8fgltlm0CnJTH8Nwg&index=2)
+- [Code]()
---
+++ System Design (gRPC, GaphQL and REST)/code/002/EX-001/examples/interoperability/reader/main.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+# XXX: In the real world a Ruby gem would be generated and required instead
+require_relative File.join("..", "..", "..", "gen", "ruby", "user", "v1", "user_pb")
+
+
+# ruby reader/main.rb from Interoperability dir
+def main
+  message = User::V1::User.decode(File.read("user.bin"))
+
+  puts message.inspect
+end
+
+main
